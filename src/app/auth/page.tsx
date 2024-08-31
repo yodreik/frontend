881c28ba3951e29fa2b70b1687e275587d0bf95e
jus1d <artfa63@gmail.com>
2024-09-01 02:54:59 +0400

    chore: Remove styles that centers everything from `globals.css`

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -19,32 +19,36 @@ const AuthPage = () => {
   };
 
   return (
-    <article className={styles.container}>
-      <div className={styles.block}>
-        <section className={styles.block_item}>
-          <h2 className={styles.block_item__title}>Already have an account?</h2>
-          <Button
-            label="Sign in"
-            onClick={handleSignInClick}
-            disabled={false}
-          />
-        </section>
-        <section className={styles.block_item}>
-          <h2 className={styles.block_item__title}>Don't have an account?</h2>
-          <Button
-            label="Sign up"
-            onClick={handleSignUpClick}
-            disabled={false}
-          />
-        </section>
-      </div>
+    <div className={styles.screen}>
+      <article className={styles.container}>
+        <div className={styles.block}>
+          <section className={styles.block_item}>
+            <h2 className={styles.block_item__title}>
+              Already have an account?
+            </h2>
+            <Button
+              label="Sign in"
+              onClick={handleSignInClick}
+              disabled={false}
+            />
+          </section>
+          <section className={styles.block_item}>
+            <h2 className={styles.block_item__title}>Don't have an account?</h2>
+            <Button
+              label="Sign up"
+              onClick={handleSignUpClick}
+              disabled={false}
+            />
+          </section>
+        </div>
 
-      <div className={`${styles.form_box} ${isSingIn ? "" : styles.active}`}>
-        <Login />
+        <div className={`${styles.form_box} ${isSingIn ? "" : styles.active}`}>
+          <Login />
 
-        <Register />
-      </div>
-    </article>
+          <Register />
+        </div>
+      </article>
+    </div>
   );
 };
 
