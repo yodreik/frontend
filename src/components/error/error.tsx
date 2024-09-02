44050ffadf9c16ebb18cca4c98bd79d1fb864c56
jus1d <artfa63@gmail.com>
2024-09-02 18:40:12 +0400

    chore: Add `ErrorPage` component

---
+++ src/components/error/error.tsx
@@ -0,0 +1,26 @@
+import Header from "../header/header";
+import styles from "./error.module.css";
+
+interface Props {
+    title: string;
+    icon?: string;
+    message?: string;
+}
+
+// TODO: Change props.icon to something like svg image
+const ErrorPage: React.FC<Props> = (props: Props) => {
+    return <>
+        <div className={styles.screen}>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.title_container}>
+                    <div className={styles.title}>{props.title}</div>
+                    <div className={styles.icon}>{props.icon}</div>
+                </div>
+                <div className={styles.message}>{props.message}</div>
+            </div>
+        </div>
+    </>;
+}
+
+export default ErrorPage;
