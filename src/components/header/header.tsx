6d5680ddbe8c6e130706e2d3fee81a10e90d06a4
jus1d <artfa63@gmail.com>
2024-09-02 21:38:36 +0400

    chore: Add `Header` layout for authorized user

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,33 +1,62 @@
 import Button from "../button/button";
 import styles from "./header.module.css";
 
+interface User {
+    id: string;
+    name: string;
+    avatarURL: string;
+}
+
 const Header = () => {
-  return (
-    <>
-      <div className={styles.header}>
-        <div className={styles.inner}>
-          <div className={styles.logo}>welnex</div>
-          <div className={styles.buttons}>
-            <Button
-              className={styles.loginButton}
-              label="Log In"
-              onClick={() => console.log("clicked log in")}
-              disabled={false}
-            />
-            <Button
-              className={styles.registerButton}
-              label="Register"
-              onClick={() => console.log("clicked register")}
-              disabled={false}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.separatorContainer}>
-        <hr className={styles.separator} />
-      </div>
-    </>
-  );
+    const user: User = {
+        id: "69",
+        name: "Donald M. Russel",
+        avatarURL: "https://img.freepik.com/free-photo/left-sideways-american-black-person_23-2148749585.jpg",
+    }
+
+    const isAuthorized = true;
+
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.inner}>
+                    <div className={styles.logo}>welnex</div>
+                    <div className={isAuthorized ? styles.buttonsAuthorized : styles.buttons}>
+                        {
+                            isAuthorized ? <>
+                                <Button
+                                    className={styles.dashboardButton}
+                                    label="Dashboard"
+                                    onClick={() => console.log("clicked dashboard")}
+                                    disabled={false}
+                                />
+                                <div className={styles.name}>
+                                    {user.name}
+                                </div>
+                                <img src={user.avatarURL} className={styles.avatar} />
+                            </> : <>
+                                <Button
+                                    className={styles.loginButton}
+                                    label="Log In"
+                                    onClick={() => console.log("clicked log in")}
+                                    disabled={false}
+                                />
+                                <Button
+                                    className={styles.registerButton}
+                                    label="Register"
+                                    onClick={() => console.log("clicked register")}
+                                    disabled={false}
+                                />
+                            </>
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className={styles.separatorContainer}>
+                <hr className={styles.separator} />
+            </div>
+        </>
+    );
 };
 
 export default Header;
