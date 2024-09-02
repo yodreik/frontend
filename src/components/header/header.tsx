de0ef47fd312d8794b4016f913486e8e06c4a989
jus1d <artfa63@gmail.com>
2024-09-02 18:40:37 +0400

    chore: Change header to client component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,33 +1,35 @@
+"use client";
+
 import Button from "../button/button";
 import styles from "./header.module.css";
 
 const Header = () => {
-  return (
-    <>
-      <div className={styles.header}>
-        <div className={styles.inner}>
-          <div className={styles.logo}>welnex</div>
-          <div className={styles.buttons}>
-            <Button
-              className={styles.loginButton}
-              label="Log In"
-              onClick={() => console.log("clicked log in")}
-              disabled={false}
-            />
-            <Button
-              className={styles.registerButton}
-              label="Register"
-              onClick={() => console.log("clicked register")}
-              disabled={false}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.separatorContainer}>
-        <hr className={styles.separator} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.inner}>
+                    <div className={styles.logo}>welnex</div>
+                    <div className={styles.buttons}>
+                        <Button
+                            className={styles.loginButton}
+                            label="Log In"
+                            onClick={() => console.log("clicked log in")}
+                            disabled={false}
+                        />
+                        <Button
+                            className={styles.registerButton}
+                            label="Register"
+                            onClick={() => console.log("clicked register")}
+                            disabled={false}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.separatorContainer}>
+                <hr className={styles.separator} />
+            </div>
+        </>
+    );
 };
 
 export default Header;
