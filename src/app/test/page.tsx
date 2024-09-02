5cf75a00df28d6d9edec261bde6e24dc7816e2a5
Reilix <dmsdyach@mail.ru>
2024-09-02 16:40:30 +0400

    chore: small button update

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "@/components/header/header";
 import Button from "@/components/button/button";
+import style from "./page.module.css";
 
 const AuthenticatePage = () => {
   return (
@@ -10,6 +11,13 @@ const AuthenticatePage = () => {
       <div>
         <h1>Authenticate</h1>
         <Button
+          className={style.color}
+          label="Dashboard"
+          onClick={() => console.log("clicked")}
+          disabled={true}
+        />
+        <Button
+          className={style.color}
           label="Dashboard"
           onClick={() => console.log("clicked")}
           disabled={false}
