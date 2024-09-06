4af108b414867c86c1e87ed8309225fe0c188cc4
Re1l1x <reilixtech@gmail.com>
2024-09-07 00:03:10 +0400

    chore: sideMenu

--- src/components/sideMenu/sideMenu.tsx
+++ src/components/sideMenu/sideMenu.tsx
@@ -1,24 +1,92 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from '@/context/AuthContext';
+import Image from "next/image";
 import styles from "./sideMenu.module.css";
+
 
 interface Props {
     isActive: boolean;
+    setInactive: () => void;
+    name: string;
+    email: string;
+    avatarURL: string;
 }
 
 const SideMenu = (props: Props) => {
-    // const [isActive, setIsActive] = useState<boolean>(false);
-    useEffect(() => {
-        if (props.isActive){
-            console.log("33213123")
-        }
-    }, [props.isActive])
+    const { setIsAuthorized } = useAuth();
+
+    const router = useRouter();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+		setIsAuthorized(false);
+        props.setInactive;
+    }
 
     return (
         <div className={styles.container}>
-            <div className={`${styles.menu} ${props.isActive ? "" : styles.active}`}>
+            <div className={`${styles.menu} ${props.isActive ? styles.active : ""}`}>
+                <button className={styles.closeButton} onClick={props.setInactive}>
+                    <Image src="/iconClose.svg" alt="Close" width={24} height={24} />
+                </button>
+
+                <div className={styles.miniProfile}>
+                    <img src={props.avatarURL} className={styles.avatar} />
+                    <div className={styles.userInfo}>
+                        <div className={styles.userName}>{props.name}</div>
+                        <div className={styles.userEmail}>{props.email}</div>
+                    </div>
+                </div>
                 
+                <hr className={styles.separator} />
+
+                <div className={styles.itemsContainer}>
+                    <button className={styles.item} onClick={() => {router.push("/")}}>
+                        <Image src="/iconProfile.svg" alt="Profile" width={24} height={24} />
+                        <div className={styles.item_title}>Profile</div>
+                    </button>
+
+                    <button className={styles.item} onClick={() => {router.push("/")}}>
+                        <Image src="/iconDashboard.svg" alt="Dashboard" width={24} height={24} />
+                        <div className={styles.item_title}>Dashboard</div>
+                    </button>
+
+                    <button className={styles.item} onClick={() => {router.push("/")}}>
+                        <Image src="/iconSettings.svg" alt="Settings" width={24} height={24} />
+                        <div className={styles.item_title}>Settings</div>
+                    </button>
+                </div>
+
+                <hr className={styles.separator} />
+
+                <div className={styles.itemsContainer}>
+                    <button className={styles.item} onClick={handleLogout}>
+                        <Image src="/iconExit.svg" alt="Exit" width={24} height={24} />
+                        <div className={styles.item_titleRed}>Sign Out</div>
+                    </button>
+                </div>
+
+                
+                <div className={styles.bottom}>
+                    <div className={styles.itemsContainer}>
+                        <button className={styles.item} onClick={() => {window.open("https://github.com/welnex", "_blank")}}>
+                            <Image src="/iconGitHub.svg" alt="GitHub" width={24} height={24} />
+                            <div className={styles.item_title}>Project on GitHub</div>
+                        </button>
+                    </div>
+
+                    <hr className={styles.separator} />
+
+                    <div className={styles.itemsContainer}>
+                        <button className={styles.item} onClick={() => {window.open("https://github.com/welnex/frontend/issues/new", "_blank")}}>
+                            <Image src="/iconBug.svg" alt="Bug" width={24} height={24} />
+                            <div className={styles.item_title}>Report a Bug</div>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
