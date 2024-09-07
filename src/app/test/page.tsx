d666d8bd84c3a7b57d5c80a08f9c9bf27573ec99
Re1l1x <reilixtech@gmail.com>
2024-09-07 17:21:39 +0400

    fix: tabs in files

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,96 +6,96 @@ import Input from "@/components/input/input";
 import { ChangeEvent, useState } from "react";
 
 const AuthenticatePage = () => {
-  const [email, setEmail] = useState<string>("sosat@chas.com");
-  const [inputStatus, setInputStatus] = useState<"error" | "default">("default");
+    const [email, setEmail] = useState<string>("sosat@chas.com");
+    const [inputStatus, setInputStatus] = useState<"error" | "default">("default");
 
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value;
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        let value = e.target.value;
 
-    if (value && !isValidEmail(value)) {
-      setInputStatus("error");
-    } else {
-      setInputStatus("default");
+        if (value && !isValidEmail(value)) {
+            setInputStatus("error");
+        } else {
+            setInputStatus("default");
+        }
+
+        setEmail(e.target.value);
+    };
+
+    const isValidEmail = (value: string): boolean => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(value);
     }
 
-    setEmail(e.target.value);
-  };
+    const handle = () => {
+        const token = localStorage.removeItem('token');
+    }
+    return (
+        <>
+            <div className={style.test}>
+                <h1>Authenticate</h1>
+                <Button
+                className={style.color}
+                label="Dashboard"
+                onClick={handle}
+                disabled={false}
+                />
+                <Button
+                className={style.color}
+                label="Dashboard"
+                onClick={() => console.log("clicked")}
+                disabled={false}
+                />
 
-  const isValidEmail = (value: string): boolean => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(value);
-  }
+                <Input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                status={inputStatus}
+                placeholder="Email"
+                disabled={true}
+                />
 
-  const handle = () => {
-    const token = localStorage.removeItem('token');
-  }
-  return (
-    <>
-      <div className={style.test}>
-        <h1>Authenticate</h1>
-        <Button
-          className={style.color}
-          label="Dashboard"
-          onClick={handle}
-          disabled={false}
-        />
-        <Button
-          className={style.color}
-          label="Dashboard"
-          onClick={() => console.log("clicked")}
-          disabled={false}
-        />
-
-        <Input
-          value={email}
-          onChange={handleEmailChange}
-          type="email"
-          status={inputStatus}
-          placeholder="Email"
-          disabled={true}
-        />
-
-        <Input
-          value={email}
-          onChange={handleEmailChange}
-          type="email"
-          status={inputStatus}
-          placeholder="Email"
-          disabled={false}
-        />
-        <Input
-          value={email}
-          onChange={handleEmailChange}
-          type="email"
-          status={inputStatus}
-          placeholder="Email"
-          disabled={false}
-        />
-        <Input
-          value={email}
-          onChange={handleEmailChange}
-          type="email"
-          status={inputStatus}
-          placeholder="Email"
-          disabled={false}
-        />
-        <Input
-          value={email}
-          onChange={handleEmailChange}
-          type="email"
-          status={inputStatus}
-          placeholder="Email"
-          disabled={false}
-        />
-        <Button
-          className={style.testb}
-          label="Dashboard"
-          onClick={handle}
-          disabled={false}
-        />
-      </div>
-    </>
-  );
+                <Input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                status={inputStatus}
+                placeholder="Email"
+                disabled={false}
+                />
+                <Input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                status={inputStatus}
+                placeholder="Email"
+                disabled={false}
+                />
+                <Input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                status={inputStatus}
+                placeholder="Email"
+                disabled={false}
+                />
+                <Input
+                value={email}
+                onChange={handleEmailChange}
+                type="email"
+                status={inputStatus}
+                placeholder="Email"
+                disabled={false}
+                />
+                <Button
+                className={style.testb}
+                label="Dashboard"
+                onClick={handle}
+                disabled={false}
+                />
+            </div>
+        </>
+    );
 };
 
 export default AuthenticatePage;
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -5,29 +5,29 @@ import "../../styles/fonts.css";
 import { ChangeEvent } from "react";
 
 interface Props {
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  type?: string;
-  status?: "error" | "default";
-  placeholder?: string;
-  disabled?: boolean;
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+    status?: "error" | "default";
+    placeholder?: string;
+    disabled?: boolean;
 }
 
 const Input = (props: Props) => {
-  return (
-    <input
-      className={
-        props.status === "error"
-          ? `${styles.input} ${styles.inputError}`
-          : `${styles.input}`
-      }
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange}
-      disabled={props.disabled}
-    />
-  );
+    return (
+        <input
+            className={
+                props.status === "error"
+                ? `${styles.input} ${styles.inputError}`
+                : `${styles.input}`
+            }
+            type={props.type}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}
+            disabled={props.disabled}
+        />
+    );
 };
 
 export default Input;
