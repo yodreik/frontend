0ca3f9eceacc35601cdab36e22813174171efd0a
jus1d <artfa63@gmail.com>
2024-09-11 17:52:41 +0400

    chore: Add `toast` component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,96 +4,17 @@ import Button from "@/components/button/button";
 import style from "./page.module.css";
 import Input from "@/components/input/input";
 import { ChangeEvent, useState } from "react";
+import Toast from "@/components/toast/toast";
+import React from "react";
 
 const AuthenticatePage = () => {
-    const [email, setEmail] = useState<string>("sosat@chas.com");
-    const [inputStatus, setInputStatus] = useState<"error" | "default">("default");
-
-    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-        let value = e.target.value;
-
-        if (value && !isValidEmail(value)) {
-            setInputStatus("error");
-        } else {
-            setInputStatus("default");
-        }
-
-        setEmail(e.target.value);
-    };
-
-    const isValidEmail = (value: string): boolean => {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(value);
-    }
-
-    const handle = () => {
-        const token = localStorage.removeItem('token');
-    }
     return (
         <>
-            <div className={style.test}>
-                <h1>Authenticate</h1>
-                <Button
-                className={style.color}
-                label="Dashboard"
-                onClick={handle}
-                disabled={false}
-                />
-                <Button
-                className={style.color}
-                label="Dashboard"
-                onClick={() => console.log("clicked")}
-                disabled={false}
-                />
-
-                <Input
-                value={email}
-                onChange={handleEmailChange}
-                type="email"
-                status={inputStatus}
-                placeholder="Email"
-                disabled={true}
-                />
-
-                <Input
-                value={email}
-                onChange={handleEmailChange}
-                type="email"
-                status={inputStatus}
-                placeholder="Email"
-                disabled={false}
-                />
-                <Input
-                value={email}
-                onChange={handleEmailChange}
-                type="email"
-                status={inputStatus}
-                placeholder="Email"
-                disabled={false}
-                />
-                <Input
-                value={email}
-                onChange={handleEmailChange}
-                type="email"
-                status={inputStatus}
-                placeholder="Email"
-                disabled={false}
-                />
-                <Input
-                value={email}
-                onChange={handleEmailChange}
-                type="email"
-                status={inputStatus}
-                placeholder="Email"
-                disabled={false}
-                />
-                <Button
-                className={style.testb}
-                label="Dashboard"
-                onClick={handle}
-                disabled={false}
-                />
-            </div>
+            <Toast
+                type={"error"}
+                title={"Ooops!"}
+                message={"User with this email already exists"}
+            />
         </>
     );
 };
