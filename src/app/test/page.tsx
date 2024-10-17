51af8d602ea839cba239010aa3ede0cf9d8b74f0
jus1d <artfa63@gmail.com>
2024-10-18 01:34:29 +0400

    chore: Add test modal window

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,46 +1,129 @@
-"use client";
+"use client"
 
-import { useToast } from "@/context/ToastContext";
+import { useState } from "react";
 import Button from "@/components/Button/Button";
-import React from "react";
+import Modal from "@/components/Modal/Modal";
+import * as Api from "@/api";
 
-const AuthenticatePage = () => {
-    const { success } = useToast();
+const Test: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [formData, setFormData] = useState({
+        date: "",
+        duration: 0,
+        kind: "",
+    });
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        let result = await Api.workout.create({ date: formData.date, duration: formData.duration, kind: formData.kind })
+
+
+
+        console.log(result);
+
+
+        console.log(`Date: ${formData.date}, duration: ${formData.duration}, kind: ${formData.kind}`)
+        e.preventDefault();
+        setIsModalOpen(false);
+    };
 
     return (
-        <>
+        <div>
             <Button
-                label="Success1"
-                onClick={
-                    () => success("HUI1", "hui2")
-                }
+                label="Add"
+                onClick={handleOpenModal}
             />
-            <Button
-                label="Success2"
-                onClick={
-                    () => success("HUI2", "hui2")
-                }
-            />
-            <Button
-                label="Success3"
-                onClick={
-                    () => success("HUI3", "hui2")
-                }
-            />
-            <Button
-                label="Success4"
-                onClick={
-                    () => success("HUI4", "hui2")
-                }
-            />
-            <Button
-                label="Success5"
-                onClick={
-                    () => success("HUI5", "hui2")
-                }
-            />
-        </>
+
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                <h2>Form Inside Modal</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="date">Date: </label>
+                        <input
+                            type="date"
+                            id="date"
+                            name="date"
+                            required
+                            value={formData.date}
+                            onChange={handleChange}
+                            pattern="\d{2}-\d{2}-\d{4}"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="duration">Duration: </label>
+                        <input
+                            type="number"
+                            id="duration"
+                            name="duration"
+                            required
+                            min="1"
+                            value={formData.duration}
+                            onChange={handleChange}
+                            placeholder="Enter positive numbers"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="kind">Kind: </label>
+                        <input
+                            type="text"
+                            id="kind"
+                            name="kind"
+                            required
+                            value={formData.kind}
+                            onChange={handleChange}
+                            placeholder="Enter the type"
+                        />
+                    </div>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </Modal>
+
+            <style jsx>{`
+        button {
+          background-color: #0070f3;
+          border: none;
+          padding: 10px 20px;
+          color: white;
+          cursor: pointer;
+          border-radius: 5px;
+        }
+
+        form {
+          display: flex;
+          flex-direction: column;
+          gap: 15px;
+        }
+
+        input {
+          padding: 5px;
+          border-radius: 4px;
+          border: 1px solid #ccc;
+        }
+
+        label {
+          margin-right: 10px;
+        }
+      `}</style>
+        </div>
     );
 };
 
-export default AuthenticatePage;
+export default Test;
