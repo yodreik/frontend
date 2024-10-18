6b3f54b687323e9ecf564565bb6af6fe68bff39c
jus1d <artfa63@gmail.com>
2024-10-18 23:37:22 +0400

    chore: Move create workout form to dashboard

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -65,75 +65,46 @@ const Test: React.FC = () => {
             />
 
             <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                <div>
-                    <div className={styles.modalInnerContainer}>
-                        <h2 className={styles.modalTitle}>Create a workout</h2>
-                        <div className={styles.formElement}>
-                            <Input
-                                value={dateValue}
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => setDateValue(e.target.value)}
-                                type="date"
-                                status={"default"}
-                                placeholder="Date"
-                            />
-                        </div>
+                <div className={styles.modalInnerContainer}>
+                    <h2 className={styles.modalTitle}>Create a workout</h2>
+                    <div className={styles.formElement}>
+                        <Input
+                            value={dateValue}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setDateValue(e.target.value)}
+                            type="date"
+                            status={"default"}
+                            placeholder="Date"
+                        />
+                    </div>
 
-                        <div className={styles.formElement}>
-                            <Input
-                                value={durationValue}
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => setDurationValue(e.target.value)}
-                                type="number"
-                                status={"default"}
-                                placeholder="Duration"
-                            />
-                        </div>
+                    <div className={styles.formElement}>
+                        <Input
+                            value={durationValue}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setDurationValue(e.target.value)}
+                            type="number"
+                            status={"default"}
+                            placeholder="Duration"
+                        />
+                    </div>
 
-                        <div className={styles.formElement}>
-                            <Input
-                                value={kindValue}
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => setKindValue(e.target.value)}
-                                type="text"
-                                status={"default"}
-                                placeholder="Kind"
-                            />
-                        </div>
+                    <div className={styles.formElement}>
+                        <Input
+                            value={kindValue}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setKindValue(e.target.value)}
+                            type="text"
+                            status={"default"}
+                            placeholder="Kind"
+                        />
+                    </div>
 
-                        <div className={styles.formElement}>
-                            <Button
-                                label="Submit"
-                                onClick={handleSubmit}
-                            />
-                        </div>
+                    <div className={styles.formElement}>
+                        <Button
+                            label="Submit"
+                            onClick={handleSubmit}
+                        />
                     </div>
                 </div>
             </Modal>
-
-            <style jsx>{`
-        button {
-          background-color: #0070f3;
-          border: none;
-          padding: 10px 20px;
-          color: white;
-          cursor: pointer;
-          border-radius: 5px;
-        }
-
-        form {
-          display: flex;
-          flex-direction: column;
-          gap: 15px;
-        }
-
-        input {
-          padding: 5px;
-          border-radius: 4px;
-          border: 1px solid #ccc;
-        }
-
-        label {
-          margin-right: 10px;
-        }
-      `}</style>
         </div>
     );
 };
