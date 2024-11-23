64a30622897ca323ddfcb94e4f258c225f7a3a70
Re1l1x <reilixtech@gmail.com>
2024-11-24 00:23:36 +0400

    chore: some fixes and add colors

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
 
-type Theme = 'light' | 'dark';
+type Theme = "light" | "dark";
 
 interface ThemeContextType {
     theme: Theme;
@@ -15,43 +15,39 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
     const [theme, setTheme] = useState<Theme>("light");
 
     useEffect(() => {
-        const savedTheme = localStorage.getItem('theme') as Theme;
+        const savedTheme = localStorage.getItem("theme") as Theme;
         if (savedTheme) {
             setTheme(savedTheme);
         } else {
-            const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
-            setTheme(prefersLight ? 'light' : 'dark');
+            const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+            setTheme(prefersLight ? "light" : "dark");
         }
     }, []);
 
     useEffect(() => {
         document.body.className = theme;
-        localStorage.setItem('theme', theme);
+        localStorage.setItem("theme", theme);
     }, [theme]);
 
     useEffect(() => {
-        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
         const handleChange = (e: MediaQueryListEvent) => {
-            setTheme(e.matches ? 'dark' : 'light');
+            setTheme(e.matches ? "dark" : "light");
         };
 
-        mediaQuery.addEventListener('change', handleChange);
+        mediaQuery.addEventListener("change", handleChange);
 
         return () => {
-            mediaQuery.removeEventListener('change', handleChange);
+            mediaQuery.removeEventListener("change", handleChange);
         };
     }, []);
 
     const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
 
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+    return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
 };
 
 export const useTheme = () => {
